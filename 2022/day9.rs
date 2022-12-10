78e0803b4b0c68552ba603d2b0d6b4a72d0c607a
David St-Hilaire <sthilaire.david@gmail.com>
2022-12-09 23:17:32 -0500

    AoC 2022 day9

--- 2022/day9.rs
+++ 2022/day9.rs
@@ -1,69 +1,102 @@
 use std::fs;
 
 struct Rope {
-    head: (i32, i32),
-    tail: (i32, i32),
+    knots: Vec<(i32, i32)>,
     tail_mem: Vec<(i32, i32)>,
 }
 
-fn is_touching(r: &Rope) -> bool {
-    let dx = r.tail.0 - r.head.0;
-    let dy = r.tail.1 - r.head.1;
+fn is_touching(r: &Rope, h: usize, t: usize) -> bool {
+    let dx = r.knots[t].0 - r.knots[h].0;
+    let dy = r.knots[t].1 - r.knots[h].1;
     return dx.abs() <= 1 && dy.abs() <= 1;
 }
 
-fn fix_tail(r: &mut Rope) -> () {
-    if is_touching(r) {
+fn update(r: &mut Rope, h: usize, t: usize, is_tail: bool) -> () {
+    if is_touching(r, h, t) {
         return;
     }
-    let dx = r.head.0 - r.tail.0;
-    let dy = r.head.1 - r.tail.1;
+    let dx = r.knots[h].0 - r.knots[t].0;
+    let dy = r.knots[h].1 - r.knots[t].1;
     if dx != 0 && dy != 0 {
-        r.tail.0 += dx.signum();
-        r.tail.1 += dy.signum();
+        r.knots[t].0 += dx.signum();
+        r.knots[t].1 += dy.signum();
     } else {
         if dx.abs() > 1 {
-            r.tail.0 += dx.signum();
+            r.knots[t].0 += dx.signum();
         } else if dy.abs() > 1 {
-            r.tail.1 += dy.signum();
+            r.knots[t].1 += dy.signum();
         }
     }
-    if !r.tail_mem.contains(&r.tail) {
-        r.tail_mem.push(r.tail);
+    if is_tail && !r.tail_mem.contains(&r.knots[t]) {
+        r.tail_mem.push(r.knots[t]);
     }
 }
 
 fn move_rope(r: &mut Rope, dx: i32, dy: i32) -> () {
-    r.head.0 += dx;
-    r.head.1 += dy;
-    while !is_touching(r) {
-        fix_tail(r);
+    let delta = if dx != 0 { dx } else { dy };
+    for _ in 0..delta.abs() {
+        if dx != 0 {
+            r.knots[0].0 += dx.signum();
+        } else {
+            r.knots[0].1 += dy.signum();
+        }
+        for t in 1..r.knots.len() {
+            let h = t - 1;
+            while !is_touching(r, h, t) {
+                update(r, h, t, t == r.knots.len() - 1);
+            }
+        }
     }
-    println!(
-        "|rope| head: {},{} tail: {}, {}",
-        r.head.0, r.head.1, r.tail.0, r.tail.1
-    );
+    // let mut msg: String = "|rope|".to_owned();
+    // for i in 0..r.knots.len() {
+    //     msg.push_str(&format!(
+    //         " knots[{}]: {},{} |",
+    //         i, r.knots[i].0, r.knots[i].1
+    //     ));
+    // }
+    // println!("{}", msg);
 }
 
-fn main() {
-    let mut rope = Rope {
-        head: (0, 0),
-        tail: (0, 0),
-        tail_mem: vec![(0, 0)],
-    };
-    let data = fs::read_to_string("day9.input").expect("Should have been able to read the file");
+fn simulate(rope: &mut Rope, data: &str) -> () {
     let lines = data.split("\n");
     for line in lines {
         let linedata: Vec<&str> = line.split(" ").collect();
         let delta = linedata[1].parse::<i32>().unwrap();
         match linedata[0] {
-            "U" => move_rope(&mut rope, 0, delta),
-            "D" => move_rope(&mut rope, 0, -delta),
-            "L" => move_rope(&mut rope, -delta, 0),
-            "R" => move_rope(&mut rope, delta, 0),
+            "U" => move_rope(rope, 0, delta),
+            "D" => move_rope(rope, 0, -delta),
+            "L" => move_rope(rope, -delta, 0),
+            "R" => move_rope(rope, delta, 0),
             _ => panic!("unhandled command"),
         }
-        //println!("{}", line);
     }
+}
+
+fn main() {
+    let data = fs::read_to_string("day9.input").expect("Should have been able to read the file");
+
+    let mut rope = Rope {
+        knots: vec![(0, 0), (0, 0)],
+        tail_mem: vec![(0, 0)],
+    };
+    simulate(&mut rope, &data);
     println!("part1: {}", rope.tail_mem.len());
+
+    let mut long_rope = Rope {
+        knots: vec![
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (0, 0),
+        ],
+        tail_mem: vec![(0, 0)],
+    };
+    simulate(&mut long_rope, &data);
+    println!("part2: {}", long_rope.tail_mem.len());
 }
